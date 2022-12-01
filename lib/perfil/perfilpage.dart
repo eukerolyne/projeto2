04f778a3d71d9179db77a96a86db47a4ea3c1d8a
Kerolyne Dias <ribeirodias2003@gmail.com>
2022-12-01 09:58:43 -0300

    perf

--- lib/perfil/perfilpage.dart
+++ lib/perfil/perfilpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:projeto2/calendario/calendario.dart';
 
 class PefilPage extends StatelessWidget {
   const PefilPage({Key? key}) : super(key: key);
